655fb9b78fea050c3ec01d9fe881d0be94495b96
magang <magang@magang-PC.fritz.box>
2017-02-06 15:27:28 +0100

    First Draft

---
+++ MultiRequestsServletDemo/src/main/java/com/maggioni/controller/MultiRequestsController.java
@@ -0,0 +1,24 @@
+package com.maggioni.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@WebServlet(urlPatterns = {"MultiRequests"})
+public class MultiRequestsController extends HttpServlet{
+
+    private static Logger logger = (Logger) LoggerFactory.getLogger("root.MultiRequestsController");
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        logger.info("Info Message");
+    }
+    
+    
+    
+}
